a2c1dc92c87a464b14222739ebd11a30d8cb96ba
Andre Wolff <andre.wolff@rede.ulbra.br>
2019-08-13 09:27:29 -0300

    Salvando02

--- Gerente.cs
+++ Gerente.cs
@@ -2,6 +2,19 @@ namespace aula02
 {
     public class Gerente
     {
-        
+        string nome = "André Guilherme Wolff";
+ double salario = 1500;
+  
+ void aumentarSalario(){
+      
+     this.salario += this.salario * 0.1;
+      
+ }
+     
+    void aumentarSalarioVariavel (double taxa){
+         
+        this.salario += this.salario * taxa;
     }
+     
 }
+    }
